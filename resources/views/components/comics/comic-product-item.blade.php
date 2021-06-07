0814cb386f98e0eb0fed4313dd7e7c284b7fae03
cristian-corbalan <ccorbalandw@gmail.com>
2021-06-07 16:54:10 -0300

    fixed discount formula

--- resources/views/components/comics/comic-product-item.blade.php
+++ resources/views/components/comics/comic-product-item.blade.php
@@ -1,22 +1,31 @@
 <div>
     <article class="comic-item">
         <div class="img-container">
-            <img src="{{ asset('storage/img/'. $comic->cover->src ) }}" alt="Portada de {{ $comic->cover->alt }}">
+            <a href="{{route('comics.details', ['comic' => $comic->id])}}">
+                <img src="{{ asset('storage/img/'. $comic->cover->src ) }}" alt="Portada de {{ $comic->cover->alt }}">
+            </a>
         </div>
+
         <div>
             <p>{{$comic->brand->name}}</p>
-            <p>{{$comic->title}}</p>
+            <p>
+                <a href="{{route('comics.details', ['comic' => $comic->id])}}">
+                    {{$comic->title}}
+                </a>
+            </p>
         </div>
+
         <div>
             @if($comic->discount)
-                <p>USD$ {{(($comic->price * $comic->discount) / 100) / 100}}</p>
+                <p>USD$ {{($comic->price - ($comic->price*$comic->discount/100))  / 100}}</p>
             @else
                 <p>USD$ {{$comic->price / 100}}</p>
             @endif
-                <button class="button">
-                    Añadir
-                    <span class="icon-shopping-cart"></span>
-                </button>
+
+            <button class="button">
+                Añadir
+                <span class="icon-shopping-cart"></span>
+            </button>
         </div>
     </article>
 </div>
